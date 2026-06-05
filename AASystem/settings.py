24719deb5f48d9914e950969d30c35a3257063ed
NASIR ABBAS <85347981+nasirabbas827@users.noreply.github.com>
2026-06-05 11:35:00 +0500

    security: remove exposed credentials

--- AASystem/settings.py
+++ AASystem/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jy8c-n9y=pf##!2^jae-l_5iafq6q%wfq8gdb6c0r5d52su+9y'
+SECRET_KEY = "YOUR_OWN_API_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
